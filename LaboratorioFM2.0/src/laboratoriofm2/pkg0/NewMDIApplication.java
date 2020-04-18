4750fa7084d0bc0f0c0dc7a92f4d99aaea60ee08
zuzu-ai <sucelyalvarez@hotmail.com>
2020-04-17 23:09:12 -0600

    Actualización RamaProceso

--- LaboratorioFM2.0/src/laboratoriofm2/pkg0/NewMDIApplication.java
+++ LaboratorioFM2.0/src/laboratoriofm2/pkg0/NewMDIApplication.java
@@ -247,8 +247,6 @@ public class NewMDIApplication extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
