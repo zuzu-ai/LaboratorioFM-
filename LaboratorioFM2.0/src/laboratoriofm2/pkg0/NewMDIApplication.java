46d777141f29c2af401d19e33b66ab29e81c29cd
zuzu-ai <sucelyalvarez@hotmail.com>
2020-04-17 23:02:00 -0600

    Merge Proceso

--- LaboratorioFM2.0/src/laboratoriofm2/pkg0/NewMDIApplication.java
+++ LaboratorioFM2.0/src/laboratoriofm2/pkg0/NewMDIApplication.java
@@ -247,6 +247,8 @@ public class NewMDIApplication extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
